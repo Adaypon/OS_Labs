26157824e42b3b76ed235a0e16952d210d196e1d
Adaypon <75831434+Adaypon@users.noreply.github.com>
2022-12-11 16:57:12 +0300

    fix: destroying shared memory when signal occurs

--- lab_3/src/prog1.c
+++ lab_3/src/prog1.c
@@ -9,14 +9,27 @@
 #include <unistd.h>
 #include <time.h>
 #include <errno.h>
+#include <signal.h>
 
 #define SHM_NAME "shmem_file"
 #define SHM_SIZE 64
 
+int g_shmid = 0;
+
+void signalHandler() {
+	if (g_shmid) {
+		printf("Destroying shared memory segment\n");
+		shmctl(g_shmid, IPC_RMID, NULL);
+	}
+	exit(0);
+}
 
 int main(int argc, char** argv) {
 	(void)argc;
 	(void)argv;
+
+	signal(SIGINT, signalHandler);
+	signal(SIGTERM, signalHandler);
 
 	struct timespec ts;
 	struct stat buffer;
@@ -31,18 +44,18 @@ int main(int argc, char** argv) {
 	}
 
 	key_t shm_key = ftok(SHM_NAME, 1);
-	int shmid = shmget(shm_key, SHM_SIZE, IPC_CREAT | IPC_EXCL | 0666);
-	if (shmid == -1 && errno == EEXIST) {
+	g_shmid = shmget(shm_key, SHM_SIZE, IPC_CREAT | IPC_EXCL | 0666);
+	if (g_shmid == -1 && errno == EEXIST) {
 		fprintf(stderr, "The program is already running: %s(%d)", strerror(errno), errno);
 		exit(1);
 	}
-	if (shmid == -1) {
+	if (g_shmid == -1) {
 		fprintf(stderr, "%s(%d)\n", strerror(errno), errno);
 		exit(1);
 	}
-	printf("[first] key: %d\n[first] mem_id: %d\n", shm_key, shmid);
+	printf("[first] key: %d\n[first] mem_id: %d\n", shm_key, g_shmid);
 
-	char* shm_ptr = shmat(shmid, NULL, 0);
+	char* shm_ptr = shmat(g_shmid, NULL, 0);
 	printf("[first] shm_ptr: %p\n", shm_ptr);
 
 	while(1) {
@@ -55,12 +68,6 @@ int main(int argc, char** argv) {
 		sleep(5);
 	}
 	
-	
-	/*
-	shmdt(shm_ptr);
-
-	shmctl(shmid, IPC_RMID, NULL);
-	*/
 	
 	return 0;
 }
