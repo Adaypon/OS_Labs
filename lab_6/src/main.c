ffafed74d19d5f53e89a93aa73ec9d87897a7a84
Adaypon <75831434+Adaypon@users.noreply.github.com>
2022-12-16 15:47:13 +0300

    feat: lab_6 init

---
+++ lab_6/src/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+pthread_mutex_t g_mtx;
+pthread_cond_t g_cond = PTHREAD_COND_INITIALIZER;
+
+// FIXME readers cond_wait loop when trying read array second time
+
+char arrCounter[] = {'0', '\0'};
+
+void* writer(void* args) {
+	(void)args;
+
+	while (1) {
+		pthread_mutex_lock(&g_mtx);
+		printf("[writer] Mutex locked\n");
+
+		printf("[writer] writing to array...\n");
+		arrCounter[0] += 1;
+		sleep(1);
+		
+		pthread_cond_broadcast(&g_cond);
+		pthread_mutex_unlock(&g_mtx);
+		printf("[writer] Mutex unlocked\n");
+		sleep(5);
+	}
+
+	pthread_exit(0);
+}
+
+void* reader(void* args) {
+	(void)args;
+	
+	while (1) {
+		pthread_mutex_lock(&g_mtx);
+		printf("[reader] %lx Mutex locked\n", pthread_self());
+
+		printf("[writer] Condition wait\n");
+		pthread_cond_wait(&g_cond, &g_mtx);
+
+		printf("[reader] %lx reading array: %s\n", pthread_self(), arrCounter);
+		sleep(1);
+		
+		
+		pthread_mutex_unlock(&g_mtx);
+		printf("[reader] %lx Mutex unlocked\n", pthread_self());
+		sleep(5);
+	}
+
+	pthread_exit(0);
+}
+
+int main(int argc, char** argv) {
+	(void)argc;
+	(void)argv;
+
+	pthread_mutex_init(&g_mtx, NULL);
+	int threadsCount = 11;
+
+	pthread_t threads[threadsCount];
+	// pthread_create(&threads[0], NULL, writer, NULL);
+	for (int i = 0; i < threadsCount - 1; ++i) {
+		pthread_create(&threads[i], NULL, reader, NULL);
+	}
+	pthread_create(&threads[threadsCount - 1], NULL, writer, NULL);
+
+	for (int i = 0; i < threadsCount; ++i) {
+		pthread_join(threads[i], NULL);
+	}
+
+	pthread_cond_destroy(&g_cond);
+	pthread_mutex_destroy(&g_mtx);
+	return 0;
+}
